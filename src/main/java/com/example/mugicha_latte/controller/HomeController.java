0d7c2dabd0cefa9c641904d6c23de6d3d5b4bb25
hayashi_temma <hayashi.temma@alhinc.jp>
2025-10-30 15:35:56 +0900

    ホーム画面表示機能実装15:35//林

---
+++ src/main/java/com/example/mugicha_latte/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.example.mugicha_latte.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/home")
+    public ModelAndView showHome(){
+
+        ModelAndView mav = new ModelAndView("home");
+        return mav;
+    }
+
+}
