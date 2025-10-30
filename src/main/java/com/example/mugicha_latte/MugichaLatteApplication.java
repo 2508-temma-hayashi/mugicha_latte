b6fe2ccc7cd869b3dd1c3c7d6d20308b34b58c99
hayashi_temma <hayashi.temma@alhinc.jp>
2025-10-30 11:52:03 +0900

    first

---
+++ src/main/java/com/example/mugicha_latte/MugichaLatteApplication.java
@@ -0,0 +1,13 @@
+package com.example.mugicha_latte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MugichaLatteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MugichaLatteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mugicha_latte/MugichaLatteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mugicha_latte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MugichaLatteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
